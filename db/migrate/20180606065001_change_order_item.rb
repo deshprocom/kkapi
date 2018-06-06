18c9dbf671c614e49de526c0663ecb91d1b1cf24
breeze-yang <breeze@deshpro.com>
2018-06-06 19:27:51 +0800

    优化订单列表的sql

--- db/migrate/20180606065001_change_order_item.rb
+++ db/migrate/20180606065001_change_order_item.rb
@@ -2,10 +2,12 @@ class ChangeOrderItem < ActiveRecord::Migration[5.1]
   def up
     remove_column :shop_order_items, :refund_status
     add_column :shop_order_items, :refunded, :boolean, default: false, comment: '是否已退款'
+    add_column :shop_order_items, :variant_image, :string, default: ''
   end
 
   def down
     add_column :shop_order_items, :refund_status, :string
     remove_column :shop_order_items, :refunded
+    remove_column :shop_order_items, :variant_image
   end
 end
