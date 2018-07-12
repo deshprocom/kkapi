c39b408281d23f2bfc315ba2a6fb855de0995e9d
breeze-yang <breeze@deshpro.com>
2018-07-12 16:42:11 +0800

    酒店订单支付后，短信通知用户

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,7 +1,7 @@
 class OrderMailer < ApplicationMailer
   # 酒店订单支付后，通知相应的员工
   default to: ENV['HOTEL_STAFF_EMAILS']
-  def notice_staff_after_paid(order)
+  def notify_staff_after_paid(order)
     @order = order
     @room = order.hotel_room
     title = "用户#{order.telephone}: 已购买 #{@room.hotel.title}-#{@room.title}"
