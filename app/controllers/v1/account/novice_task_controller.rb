32e02767f3a2693a7bcc8703bfdddcd55253d4ca
Ricky <rickycxj@gmail.com>
2018-07-18 17:02:06 +0800

    去掉访问红包进度接口的逻辑

--- app/controllers/v1/account/novice_task_controller.rb
+++ app/controllers/v1/account/novice_task_controller.rb
@@ -5,8 +5,8 @@ module V1
       before_action :user_self_required
 
       def index
-        # 判断是否是新用户 新用户才有权限访问 放开这个逻辑
-        # raise_error 'access_denied' unless @current_user.new_user
+        # 判断是否是新用户 新用户才有权限访问
+        raise_error 'access_denied' unless @current_user.new_user
       end
     end
   end
