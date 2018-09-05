6bc3b8d593123e17b79fd761ee46c545be8b4fbf
breeze-yang <breeze@deshpro.com>
2018-09-05 16:32:04 +0800

    完成商户挂售酒店的功能

--- app/controllers/v1/merchant/sale_room_requests_controller.rb
+++ app/controllers/v1/merchant/sale_room_requests_controller.rb
@@ -1,9 +1,55 @@
 module V1::Merchant
-  class SaleRoomRequestController < MerchantApplicationController
+  class SaleRoomRequestsController < MerchantApplicationController
+    before_action :set_sale_request, only: [:update, :cancel]
 
-    REQUIRES_CREATE_PARAMS = %w[hotel_id room_id room_num checkin_date price].freeze
+    REQUIRES_CREATE_PARAMS = %w[hotel_id room_id room_num checkin_date price card_img].freeze
     def create
       REQUIRES_CREATE_PARAMS.each { |param| requires! param }
+      request = @current_user.sale_room_requests.build(request_params)
+      if request.save
+        render_api_success
+      else
+        render_api_error("创建失败: #{request.errors.full_messages.join(',')}")
+      end
+    end
+
+    REQUEST_TYPES =  %w[pending passed refused on_offer sold unsold].freeze
+    def index
+      requires! :request_type, values: REQUEST_TYPES
+      @requests = @current_user.sale_room_requests
+                    .send(params[:request_type])
+                    .includes(:hotel, :hotel_room)
+                    .page(params[:page]).per(params[:page_size])
+    end
+
+    def update
+      requires! :price
+      @sale_request.update(price: params[:price])
+      # TODO 如果状态为passed，应把产生的售卖日期的房间价格给修改了
+      render_api_success
+    end
+
+    def cancel
+      if @sale_request.can_cancel?
+        @sale_request.canceled!
+        render_api_success
+        # TODO 如果状态为passed，应把产生售卖日期的房间记录给删除
+      else
+        render_api_error('目前状态不允许取消')
+      end
+    end
+
+    def set_sale_request
+      @sale_request =  SaleRoomRequest.find(params[:id])
+    end
+
+    def request_params
+      params.permit(:hotel_id,
+                    :room_id,
+                    :room_num,
+                    :checkin_date,
+                    :card_img,
+                    :price)
     end
   end
 end
