e93ef870c5ca76141ce5949ad12e9ed61a5c5a10
Ricky <rickycxj@gmail.com>
2018-11-03 10:46:21 +0800

    转盘分享增加用户次数

--- db/migrate/20181102104303_add_is_wheel_to_activities.rb
+++
@@ -1,6 +0,0 @@
-class AddIsWheelToActivities < ActiveRecord::Migration[5.1]
-  def change
-    add_column(:activities, :is_wheel, :boolean, default: false, comment: '是否是大转盘活动')
-    add_index :activities, :is_wheel
-  end
-end
