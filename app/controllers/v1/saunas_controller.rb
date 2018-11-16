64579aee01a97abfde437ed53cd9ba02229d8d45
breeze-yang <breeze@deshpro.com>
2018-11-16 18:55:01 +0800

    增加可否访问桑拿的接口

--- app/controllers/v1/saunas_controller.rb
+++ app/controllers/v1/saunas_controller.rb
@@ -6,6 +6,23 @@ module V1
       @saunas = Sauna.near([params[:latitude], params[:longitude]], 5000)
     end
 
+    def access_permission
+      requires! :latitude
+      requires! :longitude
+      amap_api = AmapApi.new
+      location = amap_api.get_location("#{params[:longitude]},#{params[:latitude]}")
+      ip_location = amap_api.get_ip_location(request.env['HTTP_X_FORWARDED_FOR'])
+
+      # 高德说明：city为空，说明不在中国大陆
+      # 1853是澳门的city code
+      # 不在大陆并且city code在澳门则可以访问桑拿
+      if ip_location['city'].blank? && location['regeocode']['addressComponent']['citycode'].to_i == 1853
+        render_api_success
+      else
+        render_api_error('不在澳门')
+      end
+    end
+
     def show
       @sauna = Sauna.find(params[:id])
     end
--- config/routes.rb
+++ config/routes.rb
@@ -65,7 +65,9 @@ Rails.application.routes.draw do
       get :coupons, on: :member
       post :receive_coupon, on: :member
     end
-    resources :saunas, only: [:show, :index]
+    resources :saunas, only: [:show, :index] do
+      get :access_permission, on: :collection
+    end
 
     resources :info_types, only: [] do
       resources :infos, only: [:show, :index] do
